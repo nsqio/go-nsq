361a6068d96776fe3d1241a2bfdaf182b94650fe
Matt Reiferson <mreiferson@gmail.com>
2012-05-24 19:47:45 +0000

    move server side client code to nsq package as ServerClient; fix compile time bugs

---
+++ server_client.go
@@ -0,0 +1,93 @@
+package nsq
+
+import (
+	"encoding/binary"
+	"io"
+	"log"
+)
+
+type ServerClient struct {
+	conn  io.ReadWriteCloser
+	name  string
+	state map[string]interface{}
+}
+
+// ServerClient constructor
+func NewServerClient(conn io.ReadWriteCloser, name string) *ServerClient {
+	return &ServerClient{conn, name, make(map[string]interface{})}
+}
+
+func (c *ServerClient) String() string {
+	return c.name
+}
+
+func (c *ServerClient) GetState(key string) (interface{}, bool) {
+	val, ok := c.state[key]
+	return val, ok
+}
+
+func (c *ServerClient) SetState(key string, val interface{}) {
+	c.state[key] = val
+}
+
+// Read proxies a read from `conn`
+func (c *ServerClient) Read(data []byte) (int, error) {
+	return c.conn.Read(data)
+}
+
+// Write prefixes the byte array with a size and 
+// proxies the write to `conn`
+func (c *ServerClient) Write(data []byte) (int, error) {
+	var err error
+
+	err = binary.Write(c.conn, binary.BigEndian, int32(len(data)))
+	if err != nil {
+		return 0, err
+	}
+
+	n, err := c.conn.Write(data)
+	if err != nil {
+		return 0, err
+	}
+
+	return (n + 4), nil
+}
+
+// Close proxies the call to `conn`
+func (c *ServerClient) Close() {
+	log.Printf("CLIENT(%s): closing", c.String())
+	c.conn.Close()
+}
+
+// Handle reads data from the client, keeps state, and
+// responds.  It is executed in a goroutine.
+func (c *ServerClient) Handle(protocols map[int32]Protocol) {
+	var err error
+	var protocolVersion int32
+
+	defer c.Close()
+
+	// the client should initialize itself by sending a 4 byte sequence indicating
+	// the version of the protocol that it intends to communicate, this will allow us 
+	// to gracefully upgrade the protocol away from text/line oriented to whatever...
+	err = binary.Read(c.conn, binary.BigEndian, &protocolVersion)
+	if err != nil {
+		log.Printf("CLIENT(%s): failed to read protocol version", c.String())
+		return
+	}
+
+	log.Printf("CLIENT(%s): desired protocol %d", c.String(), protocolVersion)
+
+	prot, ok := protocols[protocolVersion]
+	if !ok {
+		c.Write([]byte(ClientErrBadProtocol.Error()))
+		log.Printf("CLIENT(%s): bad protocol version %d", c.String(), protocolVersion)
+		return
+	}
+
+	err = prot.IOLoop(c)
+	if err != nil {
+		log.Printf("ERROR: client(%s) - %s", c.String(), err.Error())
+		return
+	}
+}
