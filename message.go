642a3f9935f12cb3b747294318d730f56f4c34b4
Dieter Plaetinck <dieter@plaetinck.be>
2016-04-15 17:07:02 -0400

    DecodeMessage: skip bytes Buffer, reducing alloc overhead (#175)
    * DecodeMessage: skip bytes Buffer, reducing alloc overhead
    
    see https://github.com/nsqio/go-nsq/issues/174#issuecomment-210256321
    for more discussion and before and after
    
    * allow empty bodies + document message format
    
    as discussed in https://github.com/nsqio/go-nsq/pull/175 and on IRC

--- message.go
+++ message.go
@@ -1,10 +1,9 @@
 package nsq
 
 import (
-	"bytes"
 	"encoding/binary"
+	"errors"
 	"io"
-	"io/ioutil"
 	"sync/atomic"
 	"time"
 )
@@ -139,24 +138,27 @@ func (m *Message) WriteTo(w io.Writer) (int64, error) {
 	return total, nil
 }
 
-// DecodeMessage deseralizes data (as []byte) and creates a new Message
+// DecodeMessage deserializes data (as []byte) and creates a new Message
+// message format:
+// [x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x][x]...
+// |       (int64)        ||    ||      (hex string encoded in ASCII)           || (binary)
+// |       8-byte         ||    ||                 16-byte                      || N-byte
+// ------------------------------------------------------------------------------------------...
+//   nanosecond timestamp    ^^                   message ID                       message body
+//                        (uint16)
+//                         2-byte
+//                        attempts
 func DecodeMessage(b []byte) (*Message, error) {
 	var msg Message
 
+	if len(b) < 10+MsgIDLength {
+		return nil, errors.New("not enough data to decode valid message")
+	}
+
 	msg.Timestamp = int64(binary.BigEndian.Uint64(b[:8]))
 	msg.Attempts = binary.BigEndian.Uint16(b[8:10])
-
-	buf := bytes.NewBuffer(b[10:])
-
-	_, err := io.ReadFull(buf, msg.ID[:])
-	if err != nil {
-		return nil, err
-	}
-
-	msg.Body, err = ioutil.ReadAll(buf)
-	if err != nil {
-		return nil, err
-	}
+	copy(msg.ID[:], b[10:10+MsgIDLength])
+	msg.Body = b[10+MsgIDLength:]
 
 	return &msg, nil
 }
