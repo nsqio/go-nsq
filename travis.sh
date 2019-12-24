f9d9845fae88918695608c52a06e635b5c03cae9
Jehiah Czebotar <jehiah@gmail.com>
2019-12-24 14:58:25 -0500

    Prep 1.0.8 release; drop dep support

--- travis.sh
+++ travis.sh
@@ -6,13 +6,5 @@ wget http://bitly-downloads.s3.amazonaws.com/nsq/$NSQ_DOWNLOAD.tar.gz
 tar zxvf $NSQ_DOWNLOAD.tar.gz
 export PATH=$NSQ_DOWNLOAD/bin:$PATH
 
-go_minor_version=$(go version | awk '{print $3}' | awk -F. '{print $2}')
-if [[ $go_minor_version -gt 10 ]]; then
-    export GO111MODULE=on
-else
-    wget -O dep https://github.com/golang/dep/releases/download/v0.5.0/dep-linux-amd64
-    chmod +x dep
-    ./dep ensure
-fi
-
+export GO111MODULE=on
 ./test.sh
