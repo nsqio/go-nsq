4bc25000f242f752f7e63ec4dee1bc1f9644c36d
Nikolay Telepenin <ntelepenin@cloudlinux.com>
2022-12-22 11:51:00 +0000

    Ability to use unix socket

--- conn.go
+++ conn.go
@@ -9,7 +9,9 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"io/fs"
 	"net"
+	"os"
 	"strings"
 	"sync"
 	"sync/atomic"
@@ -59,9 +61,10 @@ type Conn struct {
 
 	config *Config
 
-	conn    *net.TCPConn
-	tlsConn *tls.Conn
-	addr    string
+	conn       net.Conn
+	tlsConn    *tls.Conn
+	addr       string
+	socketType string
 
 	delegate ConnDelegate
 
@@ -90,8 +93,13 @@ func NewConn(addr string, config *Config, delegate ConnDelegate) *Conn {
 	if !config.initialized {
 		panic("Config must be created with NewConfig()")
 	}
+	socketType := "tcp"
+	if isSocket(addr) {
+		socketType = "unix"
+	}
 	return &Conn{
-		addr: addr,
+		addr:       addr,
+		socketType: socketType,
 
 		config:   config,
 		delegate: delegate,
@@ -119,8 +127,7 @@ func NewConn(addr string, config *Config, delegate ConnDelegate) *Conn {
 // The logger parameter is an interface that requires the following
 // method to be implemented (such as the the stdlib log.Logger):
 //
-//    Output(calldepth int, s string)
-//
+//	Output(calldepth int, s string)
 func (c *Conn) SetLogger(l logger, lvl LogLevel, format string) {
 	c.logGuard.Lock()
 	defer c.logGuard.Unlock()
@@ -176,11 +183,11 @@ func (c *Conn) Connect() (*IdentifyResponse, error) {
 		Timeout:   c.config.DialTimeout,
 	}
 
-	conn, err := dialer.Dial("tcp", c.addr)
+	conn, err := dialer.Dial(c.socketType, c.addr)
 	if err != nil {
 		return nil, err
 	}
-	c.conn = conn.(*net.TCPConn)
+	c.conn = conn
 	c.r = conn
 	c.w = conn
 
@@ -218,7 +225,7 @@ func (c *Conn) Connect() (*IdentifyResponse, error) {
 func (c *Conn) Close() error {
 	atomic.StoreInt32(&c.closeFlag, 1)
 	if c.conn != nil && atomic.LoadInt64(&c.messagesInFlight) == 0 {
-		return c.conn.CloseRead()
+		return c.conn.Close()
 	}
 	return nil
 }
@@ -666,7 +673,7 @@ func (c *Conn) close() {
 	c.stopper.Do(func() {
 		c.log(LogLevelInfo, "beginning close")
 		close(c.exitChan)
-		c.conn.CloseRead()
+		c.conn.Close()
 
 		c.wg.Add(1)
 		go c.cleanup()
@@ -720,7 +727,7 @@ func (c *Conn) waitForCleanup() {
 	// this blocks until readLoop and writeLoop
 	// (and cleanup goroutine above) have exited
 	c.wg.Wait()
-	c.conn.CloseWrite()
+	c.conn.Close()
 	c.log(LogLevelInfo, "clean close complete")
 	c.delegate.OnClose(c)
 }
@@ -762,4 +769,12 @@ func (c *Conn) log(lvl LogLevel, line string, args ...interface{}) {
 	logger.Output(2, fmt.Sprintf("%-4s %s %s", lvl,
 		fmt.Sprintf(logFmt, c.String()),
 		fmt.Sprintf(line, args...)))
+}
+
+func isSocket(path string) bool {
+	fileInfo, err := os.Stat(path)
+	if err != nil {
+		return false
+	}
+	return fileInfo.Mode().Type() == fs.ModeSocket
 }
