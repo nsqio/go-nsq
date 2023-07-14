d9c722e4f446f64e42dd05d3d2c9e8ee146d9337
guoguangwu <guoguangwu@magic-shield.com>
2023-07-14 16:14:21 +0800

    chore: use time.Since instead of time.Now().Sub

--- consumer.go
+++ consumer.go
@@ -1033,8 +1033,8 @@ func (r *Consumer) redistributeRDY() {
 
 	possibleConns := make([]*Conn, 0, len(conns))
 	for _, c := range conns {
-		lastMsgDuration := time.Now().Sub(c.LastMessageTime())
-		lastRdyDuration := time.Now().Sub(c.LastRdyTime())
+		lastMsgDuration := time.Since(c.LastMessageTime())
+		lastRdyDuration := time.Since(c.LastRdyTime())
 		rdyCount := c.RDY()
 		r.log(LogLevelDebug, "(%s) rdy: %d (last message received %s)",
 			c.String(), rdyCount, lastMsgDuration)
